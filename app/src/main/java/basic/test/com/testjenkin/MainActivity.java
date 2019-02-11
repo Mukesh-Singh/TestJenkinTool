fa11862403a5cd34f6e6f2b707c4381d23e0ac1e
Mukesh <mk.singh3586@gmail.com>
2019-02-11 18:02:48 +0530

    1.Change splase message. 2.Change in login process. 3.Dialog show on loging failed.

--- app/src/main/java/basic/test/com/testjenkin/MainActivity.java
+++ app/src/main/java/basic/test/com/testjenkin/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         //Test
-        //test change log in jenkins meail
+
     }
 
 }
