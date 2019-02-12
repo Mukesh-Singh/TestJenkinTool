40fbe9f4c0d939b0a66d386c3b6fe0082f606367
Mukesh <mk.singh3586@gmail.com>
2019-02-12 12:41:58 +0530

    test changes3 test commit message jenkins [ci ciBUILD]

--- app/src/main/java/basic/test/com/testjenkin/MainActivity.java
+++ app/src/main/java/basic/test/com/testjenkin/MainActivity.java
@@ -20,11 +20,8 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
-
                     return true;
                 case R.id.navigation_dashboard:
-
-
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
