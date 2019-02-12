59fe8c81d980d63064594ef9a277b4576805aacd
Mukesh <mk.singh3586@gmail.com>
2019-02-12 17:16:21 +0530

    test change log

--- app/src/main/java/basic/test/com/testjenkin/MainActivity.java
+++ app/src/main/java/basic/test/com/testjenkin/MainActivity.java
@@ -41,8 +41,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //some issue fixing
-        //test branch testing
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
