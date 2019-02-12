8eb155cf0589ad3d0d91f9be9bab3308bf6a23d9
Mukesh <mk.singh3586@gmail.com>
2019-02-12 17:48:31 +0530

    -dialog issue fixed -remove message

--- app/src/main/java/basic/test/com/testjenkin/MainActivity.java
+++ app/src/main/java/basic/test/com/testjenkin/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //some issue fixing
         //test branch testing
+        //temp issue fixing
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
