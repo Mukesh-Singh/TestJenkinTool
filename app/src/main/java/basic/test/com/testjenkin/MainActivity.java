a0304dbcc62b4d2b229438c7bbadd146a9abb679
Mukesh <mk.singh3586@gmail.com>
2019-02-12 18:03:37 +0530

    -added new feature in temp_new_feature_branch -Show test dialog in temp_new_feature_branch

--- app/src/main/java/basic/test/com/testjenkin/MainActivity.java
+++ app/src/main/java/basic/test/com/testjenkin/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         //Test
+        //add new feature
 
     }
 
