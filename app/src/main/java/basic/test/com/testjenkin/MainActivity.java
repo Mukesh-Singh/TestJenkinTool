92eb5c0b6b4b9ca3a0b8bbcd72d0f7c00a503a32
Mukesh <mk.singh3586@gmail.com>
2019-02-14 16:07:27 +0530

    change in build process master

--- app/src/main/java/basic/test/com/testjenkin/MainActivity.java
+++ app/src/main/java/basic/test/com/testjenkin/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//TEST
         setContentView(R.layout.activity_main);
 
         //mTextMessage = (TextView) findViewById(R.id.message);
